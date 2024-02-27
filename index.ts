de0125d37780d59e36403477e5f08d3b2e9aedcc
renorari <renorari@renorari.net>
2024-02-27 23:59:09 +0900

    どっちも生成するようにした

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import cp from "node:child_process";
 import fs from "node:fs";
 
-if (!fs.existsSync("labels")) fs.mkdirSync("labels");
-if (!fs.existsSync("labels/full")) fs.mkdirSync("labels/full");
-if (!fs.existsSync("labels/mono")) fs.mkdirSync("labels/mono");
+if (!fs.existsSync("emotion-labels")) fs.mkdirSync("emotion-labels");
+if (!fs.existsSync("emotion-labels/full")) fs.mkdirSync("emotion-labels/full");
+if (!fs.existsSync("emotion-labels/mono")) fs.mkdirSync("emotion-labels/mono");
 
 fs.readFileSync("emotion_transcript_utf8.txt", "utf8").split("\n").forEach((line) => {
     if (!line.startsWith("EMOTION")) return;
@@ -11,4 +11,16 @@ fs.readFileSync("emotion_transcript_utf8.txt", "utf8").split("\n").forEach((line
     const content = line.split(":")[1].split(",")[0];
     cp.exec(`openjtalk_label_getter --dict_path /opt/homebrew/opt/open-jtalk/dic --htsvoice_path /opt/homebrew/opt/open-jtalk/voice/mei/mei_normal.htsvoice --output_type full_context_label ${content} > labels/full/${title}.lab`).toString();
     cp.exec(`openjtalk_label_getter --dict_path /opt/homebrew/opt/open-jtalk/dic --htsvoice_path /opt/homebrew/opt/open-jtalk/voice/mei/mei_normal.htsvoice --output_type phoneme ${content} > labels/mono/${title}.lab`).toString();
+});
+
+if (!fs.existsSync("recitation-labels")) fs.mkdirSync("recitation-labels");
+if (!fs.existsSync("recitation-labels/full")) fs.mkdirSync("recitation-labels/full");
+if (!fs.existsSync("recitation-labels/mono")) fs.mkdirSync("recitation-labels/mono");
+
+fs.readFileSync("recitation_transcript_utf8.txt", "utf8").split("\n").forEach((line) => {
+    if (!line.startsWith("RECITATION")) return;
+    const title = line.split(":")[0];
+    const content = line.split(":")[1].split(",")[0];
+    cp.exec(`openjtalk_label_getter --dict_path /opt/homebrew/opt/open-jtalk/dic --htsvoice_path /opt/homebrew/opt/open-jtalk/voice/mei/mei_normal.htsvoice --output_type full_context_label ${content} > recitation-labels/full/${title}.lab`).toString();
+    cp.exec(`openjtalk_label_getter --dict_path /opt/homebrew/opt/open-jtalk/dic --htsvoice_path /opt/homebrew/opt/open-jtalk/voice/mei/mei_normal.htsvoice --output_type phoneme ${content} > recitation-labels/mono/${title}.lab`).toString();
 });
