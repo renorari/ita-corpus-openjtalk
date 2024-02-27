3547f6baee8aec45f9acc206e615d758b5542aa5
renorari <renorari@renorari.net>
2024-02-27 23:56:39 +0900

    プログラムを変更

--- index.ts
+++ index.ts
@@ -9,6 +9,6 @@ fs.readFileSync("emotion_transcript_utf8.txt", "utf8").split("\n").forEach((line
     if (!line.startsWith("EMOTION")) return;
     const title = line.split(":")[0];
     const content = line.split(":")[1].split(",")[0];
-    cp.execSync(`openjtalk_label_getter --dict_path /opt/homebrew/opt/open-jtalk/dic --htsvoice_path /opt/homebrew/opt/open-jtalk/voice/mei/mei_normal.htsvoice --output_type full_context_label ${content} > labels/full/${title}.lab`).toString();
-    cp.execSync(`openjtalk_label_getter --dict_path /opt/homebrew/opt/open-jtalk/dic --htsvoice_path /opt/homebrew/opt/open-jtalk/voice/mei/mei_normal.htsvoice --output_type phoneme ${content} > labels/mono/${title}.lab`).toString();
+    cp.exec(`openjtalk_label_getter --dict_path /opt/homebrew/opt/open-jtalk/dic --htsvoice_path /opt/homebrew/opt/open-jtalk/voice/mei/mei_normal.htsvoice --output_type full_context_label ${content} > labels/full/${title}.lab`).toString();
+    cp.exec(`openjtalk_label_getter --dict_path /opt/homebrew/opt/open-jtalk/dic --htsvoice_path /opt/homebrew/opt/open-jtalk/voice/mei/mei_normal.htsvoice --output_type phoneme ${content} > labels/mono/${title}.lab`).toString();
 });
